f29ba0087cfaa17fc5b99ee91581ec99a73bff71
Charles <charles@techbeast.co>
2017-02-16 22:42:56 +0800

    Initial Commit

---
+++ TBWKWebView/TBWKWebView.h
@@ -0,0 +1,19 @@
+//
+//  TBWKWebView.h
+//  TBWKWebView
+//
+//  Created by Charles on 16/2/2017.
+//  Copyright © 2017 Tech Beast Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBWKWebView.
+FOUNDATION_EXPORT double TBWKWebViewVersionNumber;
+
+//! Project version string for TBWKWebView.
+FOUNDATION_EXPORT const unsigned char TBWKWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBWKWebView/PublicHeader.h>
+
+
